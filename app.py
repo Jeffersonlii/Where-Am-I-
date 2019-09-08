78982a9f1660a09eef357514799cc3fd725ed967
jffersn <32963293+jffersn@users.noreply.github.com>
2019-09-08 17:46:39 -0400

    added copy paste and deletion

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-import random, json, config
+from apscheduler.scheduler import Scheduler
+import random, json, config, atexit
 
 app = Flask(__name__)
 app.secret_key=b'supersecret1'
@@ -9,12 +10,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///locations.db'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0#avoids caching
 db = SQLAlchemy(app)
 
+cron = Scheduler(daemon=True)#create cron job thread for periodic deletion
+cron.start()
+
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html',uniquePath=assignUniquePath())
 
 #generates a unique path, and inserts it into the db. returns the path
-def assignUniquePath():
+def assignUniquePath():#get path
     uniquePath = 0
     unique = False #is the current path unique?
     while not unique: #make sure the url is UNIQUE
@@ -24,8 +28,8 @@ def assignUniquePath():
                 .format(uniquePath)))
         for _ in result: #if no matches, continue will not be ran 
             unique = False
-    sql='''INSERT INTO locat (path,long,lat) 
-        VALUES ({},0,0)'''.format(uniquePath) #insert the new path
+    sql='''INSERT INTO locat (path,long,lat,time) 
+        VALUES ({},0,0, CURRENT_TIMESTAMP )'''.format(uniquePath) #insert the new path
     db.engine.execute(text(sql))
     return uniquePath
 
@@ -41,7 +45,7 @@ def updateLocat():#gets locatiopn data from js ajax and updates db, updates in a
     return "Done"
 
 @app.route('/<path:uniquePath>',methods=['GET'])
-def map(uniquePath):
+def map(uniquePath):#map code
   
     sql='''select long,lat from locat where
     path = {}
@@ -53,6 +57,16 @@ def map(uniquePath):
                                         key = config.MAP_KEY) 
     return render_template('map.html',uniquePath=-1) 
 
+@cron.interval_schedule(hours=1)
+def cronDeleter():#cron process for deleting at 1 hour interval
+    sql=''' DELETE FROM locat WHERE
+    time < DATETIME('NOW', '-2 hours') '''#delete all rows created > 2 hours ago
+    db.engine.execute(text(sql))
+
+# close cron job when web app is stopped
+atexit.register(lambda: cron.shutdown(wait=False))
+
 if __name__ =="__main__":
     #SQLALCHEMY_TRACK_MODIFICATIONS = False
     app.run(host="localhost",port=5000,debug=True)
+
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+//set unique path in url
 function setUniquePath(a){
     var path = document.getElementById('urlBar');
     path.value = window.location + a;
@@ -12,17 +13,25 @@ function success(position,uniquePath) {//execute on success
     console.log("refreshed");
     
     $.post("/updateLocat",{//a post call w/o refreshing page
+        
         lat: position.coords.latitude,
         long: position.coords.longitude,
         path: uniquePath
     });
 }
-function sendCoords(uniquePath){
+function sendCoords(uniquePath){//sending coords
     if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(function(position){
+        navigator.geolocation.getCurrentPosition(function(position){//watchPosition repeats
             success(position,uniquePath);
         },function(){},options);
     } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
+}
+
+function copyToBoard(){//copy button
+  var copyText = document.getElementById("urlBar");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
